4fbcc6cac09b17063989a74b6c2e674ec53362f7
searchcoin-dev <user@users-iMac.local>
2017-11-12 14:18:22 +0900

    change

--- public/src/js/services/api.js
+++ public/src/js/services/api.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('insight.api')
-    .factory('Api',
-        function() {
-            return {
-                apiPrefix: '/insight-ssc-api'
-            }
-        });
+  .factory('Api',
+    function() {
+      return {
+        apiPrefix: '/insight-ssc-api'
+      }
+    });
